5b9b7e0bed6ca61eefb055d9ff6a219d588838eb
SaiSujith-2007 <72797938+SaiSujith-2007@users.noreply.github.com>
2020-11-18 19:06:08 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -14,6 +14,8 @@ class paper
     World.add(world,this.body);
     }
     display(){
+        imageMode(RADIUS);
+        image(this.image,this.body.position.x, this.body.position.y, this.radius, this.radius);
         fill("pink");
         ellipseMode(RADIUS);
         ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
